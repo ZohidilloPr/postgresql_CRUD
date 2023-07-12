40faee9c31c41bf4dd930cdfdb184267f17dbac5
ZohidilloPr <turgunovzohidillopr@yahoo.com>
2023-07-12 10:47:23 +0500

    delete .ini file set variables in .env

--- config/settings.py
+++ config/settings.py
@@ -1,19 +1,16 @@
-from decouple import config
+from decouple import config as conn
 from configparser import ConfigParser
 
 # encode and decode password fernet key
-FERNET_GENERATE_KEY = config("FERNET_GENERATE_KEY").encode("utf-8")
+FERNET_GENERATE_KEY = conn("FERNET_GENERATE_KEY").encode("utf-8")
 
-def config(filename="config/database.ini", section="postgresql"):
-    """ database.ini filedan malumotlarni olish uchun """
-    parser = ConfigParser()
-    parser.read(filename)
-
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception("Section {0} not found in the {1} file".format(section, filename))
-    return db
+def config():
+    """ .env filedan malumotlarni olish uchun """
+    database = {
+        "host": conn("host"),
+        "dbname": conn("dbname"),
+        "user": conn("user"),
+        "password": conn("password"),
+        "port": conn("port")
+    }
+    return database
